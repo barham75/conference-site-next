194a8661b59ba83628376ec1f6fb6d16febfc5ab
Mohammad Barham <mah.barham04777@gmail.com>
2026-02-03 15:16:32 +0300

    fix home page not found

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,16 @@
-import { redirect } from "next/navigation";
+export default function HomePage() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Conference Site</h1>
+      <p>Welcome to the conference website.</p>
 
-export default function Home() {
-  redirect("/register");
+      <ul>
+        <li><a href="/register">Register</a></li>
+        <li><a href="/program">Program</a></li>
+        <li><a href="/poster-vote">Poster Vote</a></li>
+        <li><a href="/evaluation">Evaluation</a></li>
+        <li><a href="/supporters">Supporters</a></li>
+      </ul>
+    </main>
+  );
 }
