7816e4960202edcb54134085adcb921a4c23d4cf
Mohammad Barham <mah.barham04777@gmail.com>
2026-02-03 15:24:18 +0300

    fix root page redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,5 @@
-export default function HomePage() {
-  return (
-    <main style={{ padding: 40 }}>
-      <h1>Conference Site</h1>
-      <p>Welcome to the conference website.</p>
+import { redirect } from "next/navigation";
 
-      <ul>
-        <li><a href="/register">Register</a></li>
-        <li><a href="/program">Program</a></li>
-        <li><a href="/poster-vote">Poster Vote</a></li>
-        <li><a href="/evaluation">Evaluation</a></li>
-        <li><a href="/supporters">Supporters</a></li>
-      </ul>
-    </main>
-  );
+export default function Home() {
+  redirect("/register");
 }
